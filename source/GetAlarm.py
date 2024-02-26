a367653a12e014eb97e0508512ff03875ee3af44
Ivan Tyshchenko <ivan.tyshchenko@gmail.com>
2024-02-26 13:56:42 +0200

    Get Alarm

---
+++ source/GetAlarm.py
@@ -0,0 +1,74 @@
+import json
+import requests
+
+API_KEY = " " #DONT EVER COMMIT WITH API !!!
+
+uid_dict = {"Хмельницька область": 3,
+            "Вінницька область": 4,
+            "Рівненська область": 5,
+            "Волинська область": 8,
+            "Дніпропетровська область": 9,
+            "Житомирська область": 10,
+            "Закарпатська область": 11,
+            "Запорізька область": 12,
+            "Івано-Франківська область": 13,
+            "Київська область": 14,
+            "Кіровоградська область": 15,
+            "Луганська область": 16,
+            "Миколаївська область": 17,
+            "Одеська область": 18,
+            "Полтавська область": 19,
+            "Сумська область": 20,
+            "Тернопільська область": 21,
+            "Харківська область": 22,
+            "Херсонська область": 23,
+            "Черкаська область": 24,
+            "Чернігівська область": 25,
+            "Чернівецька область": 26,
+            "Львівська область": 27,
+            "Донецька область": 28,
+            "Автономна Республіка Крим": 29,
+            "м. Севастополь": 30, "м. Київ": 31
+            }
+
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+
+        self.payload = payload
+
+    def to_dict(self):
+        res = dict(self.payload or ())
+        res["message"] = self.message
+        return res
+
+    def print(self):
+        print(self.to_dict())
+
+
+def get_alarm_by_url(location):
+    index = uid_dict.get(location)
+    url = f"https://api.alerts.in.ua/v1/iot/active_air_raid_alerts/{index}.json?token={API_KEY}"
+    response = requests.get(url)
+    if response.status_code == requests.codes.ok:
+        return json.loads(response.text)
+    else:
+        raise InvalidUsage(response.text, status_code=response.status_code)
+
+
+def get_alarm(location):
+    if location not in uid_dict.values():
+        return InvalidUsage(message=f"Invalid location - {location}").print()
+    alarm = get_alarm_by_url(location)
+    if alarm == "A":
+        return True
+    else:
+        return False
+
+loc = " "
+print(get_alarm(loc))
--- source/test_collector.py
+++ source/test_collector.py
@@ -6,10 +6,14 @@ url = "https://www.understandingwar.org/backgrounder/ukraine-conflict-updates-20
 r = requests.get(url)
 print(r.status_code)
 soup = BeautifulSoup(r.text, "html.parser")
-dates = soup.find_all('div:nth-child(1)', class_='strong')
+dates  = soup.select("#block-system-main div.field.field-name-body.field-type-text-with-summary.field-label-hidden div div div:nth-child(1)")
+
 print(dates)
 date_element = soup.select_one("#block-system-main > div > div > div.field.field-name-body.field-type-text-with-summary.field-label-hidden > div > div > div:nth-child(1) > p:nth-child(15) > strong")
 print (date_element)
+
+
+
 
 #block-system-main > div > div > div.field.field-name-body.field-type-text-with-summary.field-label-hidden > div > div > div:nth-child(1) > p:nth-child(15) > strong
 #block-system-main > div > div > div.field.field-name-body.field-type-text-with-summary.field-label-hidden > div > div > div:nth-child(1) > p:nth-child(31) > strong
