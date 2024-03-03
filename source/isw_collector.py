231ebc1f936cc083124fa750d3712954a8297b08
Nych Kateryna <134296258+catnukh@users.noreply.github.com>
2024-03-03 17:32:43 +0200

    Update isw_collector.py

--- source/isw_collector.py
+++ source/isw_collector.py
@@ -1,12 +1,10 @@
 from isw_requester import ISWRequester
-# import csv
+import csv
 import pandas as pd
 import datetime as dt
 class ISWCollector:
     def __init__(self):
-        self.urls  = []
-        self.data = pd.DataFrame(
-            columns=["url", "title", "date", "html_text", "text"])
+        self.urls = []
 
     @staticmethod
     def collect(url):
@@ -27,20 +25,18 @@ class ISWCollector:
     def generate_url_roca():
         base_url = "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment"
         date_range_generator = ISWCollector.date_range(dt.date(2022, 3, 1), dt.date(2023, 1, 26))
-        date_range_generator = ISWCollector.date_range(dt.date(2022, 3, 1), dt.date(2023, 1, 26))
-        #added url from February 25 to February 28
+        # for days from 25.02.2022 to 28.02.2022
         url_list = ["https://www.understandingwar.org/backgrounder/russia-ukraine-warning-update-russian-offensive-campaign-assessment-february-25-2022",
                     "https://www.understandingwar.org/backgrounder/russia-ukraine-warning-update-russian-offensive-campaign-assessment-february-26",
                     "https://www.understandingwar.org/backgrounder/russia-ukraine-warning-update-russian-offensive-campaign-assessment-february-27",
                     "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-february-28-2022"]
+
         for date in date_range_generator:
             if date.year == 2022:
-                #changed day-format (without 0 before number)
                 url_list.append(base_url + "-" + date.strftime("%B-%#d"))
             else:
                 url_list.append(base_url + "-" + ISWCollector.reformat_date(date))
-                
-        #list of not working urls
+
         problem_url = ["https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-May-5",
                        "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-July-11",
                        "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-August-12",
@@ -48,11 +44,10 @@ class ISWCollector:
                        "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-December-25",
                        "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-january-1-2023"]
 
-        #remove urls that caused promlems
         for url in url_list:
             if url in problem_url:
                 url_list.remove(url)
-                
+
         return url_list
 
     @staticmethod
@@ -64,7 +59,7 @@ class ISWCollector:
 
     @staticmethod
     def reformat_date(input_date):
-        formatted_date = input_date.strftime('%B-%#d-%Y').lower()
+        formatted_date = input_date.strftime('%B-%d-%Y').lower()
         return formatted_date
 
     @staticmethod
@@ -76,4 +71,45 @@ class ISWCollector:
 if __name__ == "__main__":
     isw_collector = ISWCollector()
     isw_collector.add_url(isw_collector.generate_url_roca())
-    print(isw_collector.urls)
+
+    instances = []
+
+    for i in range(len(isw_collector.urls[0])):
+        url = isw_collector.urls[0][i]
+        instances.append(ISWRequester(url))
+
+    with open("ISW.csv", "w", newline="", encoding='utf-8') as csvfile:
+        a = instances[0]
+        data_dict = a.to_dict()
+        fieldnames = list(data_dict.keys())
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for instance in instances:
+            instance.beautify()
+            data_dict = instance.to_dict()
+            writer.writerow(data_dict)
+
+    df = pd.read_csv("ISW.csv")
+
+    print(df)
+
+    df.dropna(subset=["date"], inplace=True)
+
+    print(df)
+
+    df.to_csv("ISW.csv", index=False)
+
+    isw_yesterday = ISWCollector()
+    isw_yesterday.add_url(isw_yesterday.generate_url_yesterday())
+    a = isw_yesterday.urls[0]
+    print(a)
+    isw_req = ISWRequester(a[0])
+
+    isw_req.beautify()
+    b = isw_req.to_dict()
+    data_dict = isw_req.to_dict()
+    df_yesterday = pd.DataFrame(columns=data_dict.keys())
+    df_yesterday = pd.concat([df, pd.DataFrame([b])], ignore_index=True)
+    print(df_yesterday)
