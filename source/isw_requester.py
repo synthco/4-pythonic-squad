1c859d412e28f7d926defa1d0ff60867a2cc65f7
Nych Kateryna <134296258+catnukh@users.noreply.github.com>
2024-03-03 17:29:02 +0200

    Update isw_requester.py

--- source/isw_requester.py
+++ source/isw_requester.py
@@ -6,14 +6,16 @@ import re
 class ISWRequester:
     def __init__(self, url: str):
         self.url = url
-        self.soup = self.get_soup()                       #main_html
-        self.title = self.get_title()                           #titile
-        self.date = self.get_date()                         #date
-        self.html_data = self._html_raw_parse()  #main_html_v2
+        self.soup = self.get_soup()  # main_html
+        self.title = self.get_title()  # title
+        self.date = self.get_date()  # date
+        self.html_data = self._html_raw_parse()  # main_html_v2
         self._raw_data = self._parse_raw()
-        self._data_collection = None  # main_text -> to dict
+        self._data_collection = self.to_dict()  # main_text -> to dict
 
 
+    def get_html_data(self):
+        return self.html_data
 
     @property
     def data_collection(self):
@@ -38,82 +40,76 @@ class ISWRequester:
 
     def _parse_raw(self):
         field_items_divs = self.soup.find_all("div", class_="field-items")
-        if field_items_divs:
+        res = []
+        if field_items_divs and len(field_items_divs) > 1:
             paragraphs = field_items_divs[2].find_all("p") + field_items_divs[2].find_all("ul")
-            res = []
             for paragraph in paragraphs:
                 res.append(paragraph.text.strip())
-
-        else:
-            raise Exception("No Paragraphs found")
-        return res
+            return res
+        return None
 
     def _html_raw_parse(self):
         field_items_divs = self.soup.find_all("div", class_="field-items")
-        if field_items_divs:
+        if field_items_divs and len(field_items_divs) > 1:
             paragraphs = field_items_divs[2].find_all("p")
             res = []
             for paragraph in paragraphs:
                 res.append(paragraph)
-        pass
+            return res
+        return None
 
     def raw_out(self):
-        for i in range(self.raw_data.__len__()):
-            print(self.raw_data[i])
+        for i in self.raw_data:
+            print(i)
 
     def beautify(self):
-        self.remove_links()
-        #Remove all unnecessary information
-        self.raw_data = [data for data in self.raw_data
-                         if not data.startswith("Note") and not data.startswith("Click") and data != '']
+        if self.raw_data is not None:
+            self.remove_links()
+            # Remove all unnecessary information
+            self.raw_data = [data for data in self.raw_data
+                             if not data.startswith("Note") and not data.startswith("Click") and data != '']
 
-        for i, row in enumerate(self.raw_data):
-            self.raw_data[i] = re.sub(r'\[\d+\]', '', row)
+            for i, row in enumerate(self.raw_data):
+                self.raw_data[i] = re.sub(r'\[\d+\]', '', row)
 
 
     def remove_links(self):
-        #Remove all links at the bottom of the reqest
+        # Remove all links at the bottom of the reqest
         links = [link.get('href') for link in self.soup.find_all('a') if link.get('href') is not None]
         self.raw_data = [data for data in self.raw_data if not any(link in data for link in links)]
-        
+
     def get_date(self):
         date_string = self.title.split(", ", 1)[1]
-        print(date_string)
         if "2023" in date_string:
             return dt.datetime.strptime(date_string, "%B %d, %Y").date()
         elif "2022" in date_string:
+            return dt.datetime.strptime(date_string, "%B %d, %Y").date()
+        elif "2024" in date_string:
             return dt.datetime.strptime(date_string, "%B %d, %Y").date()
         else:
             date = date_string + ", 2022"
             return dt.datetime.strptime(date, "%B %d, %Y").date()
 
-
-
-
     def get_title(self):
-        #Get title from HTML
+        # Get title from HTML
         title = self.soup.find("h1")
-
         return title.text.strip()
 
-
     def to_dict(self):
-        #Convert pure data to dict
-        #ask Andrew WTF is main_html and main_html_v2?
-        # date
-        # short_url
-        # title
-        # text_title
-        # full_url
-        # main_html
-        # main_html_v2
-        # main_text
-        pass
+        dict = {}
 
+        dict["date"] = self.date
+        dict["title"] = self.title
+        dict["full_url"] = self.url
+        dict["main_html"] = self.soup
+        dict["main_html_v2"] = self.html_data
+        dict["main_text"] = self._raw_data
+
+        return dict
 
 
 if __name__ == "__main__":
-    url = "https://understandingwar.org/backgrounder/russian-offensive-campaign-assessment-december-12"
+    url = "https://understandingwar.org/backgrounder/russian-offensive-campaign-assessment-december-13"
     isw = ISWRequester(url)
     isw.beautify()
     isw.raw_out()
