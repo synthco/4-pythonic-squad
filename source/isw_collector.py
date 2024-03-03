16aca16617a64aa624f09112cac3f2f86e6e889c
Nych Kateryna <134296258+catnukh@users.noreply.github.com>
2024-03-03 13:35:21 +0200

    Update isw_collector.py

--- source/isw_collector.py
+++ source/isw_collector.py
@@ -26,13 +26,30 @@ class ISWCollector:
     @staticmethod
     def generate_url_roca():
         base_url = "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment"
-        date_range_generator = ISWCollector.date_range(dt.date(2022, 2, 24), dt.date(2023, 1, 25))
-        url_list = []
+        date_range_generator = ISWCollector.date_range(dt.date(2022, 3, 1), dt.date(2023, 1, 26))
+        # for days from 25.02.2022 to 28.02.2022
+        url_list = ["https://www.understandingwar.org/backgrounder/russia-ukraine-warning-update-russian-offensive-campaign-assessment-february-25-2022",
+                    "https://www.understandingwar.org/backgrounder/russia-ukraine-warning-update-russian-offensive-campaign-assessment-february-26",
+                    "https://www.understandingwar.org/backgrounder/russia-ukraine-warning-update-russian-offensive-campaign-assessment-february-27",
+                    "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-february-28-2022"]
+
         for date in date_range_generator:
             if date.year == 2022:
-                url_list.append(base_url + "-" + date.strftime("%B-%d"))
+                url_list.append(base_url + "-" + date.strftime("%B-%#d"))
             else:
                 url_list.append(base_url + "-" + ISWCollector.reformat_date(date))
+
+        problem_url = ["https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-May-5",
+                       "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-July-11",
+                       "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-August-12",
+                       "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-November-24",
+                       "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-December-25",
+                       "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-january-1-2023"]
+
+        for url in url_list:
+            if url in problem_url:
+                url_list.remove(url)
+
         return url_list
 
     @staticmethod
