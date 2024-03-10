7f08b4eaaa1637bbe67df7808fefb94a8230efe8
Militokrat <107516785+Militokrat@users.noreply.github.com>
2024-03-10 13:35:18 +0200

    isw_collector.py

--- source/isw_collector.py
+++ source/isw_collector.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
         url = isw_collector.urls[0][i]
         instances.append(ISWRequester(url))
 
-    with open("ISW.csv", "w", newline="", encoding='utf-8') as csvfile:
+    with open("ISW01.csv", "w", newline="", encoding='utf-8') as csvfile:
         a = instances[0]
         data_dict = a.to_dict()
         fieldnames = list(data_dict.keys())
@@ -91,34 +91,32 @@ if __name__ == "__main__":
             data_dict = instance.to_dict()
             writer.writerow(data_dict)
 
-    df = pd.read_csv("ISW.csv")
+    df = pd.read_csv("ISW01.csv")
 
     print(df)
-
 
     df.dropna(subset=["date"], inplace=True)
 
     print(df)
 
-    df.to_csv("ISW.csv", index=False)
+    df.to_csv("ISW01.csv", index=False)
 
-    isw_yesterday = ISWCollector()
-    isw_yesterday.add_url(isw_yesterday.generate_url_yesterday())
-    a = isw_yesterday.urls[0]
-    print(a)
-    isw_req = ISWRequester(a[0])
+    # isw_yesterday = ISWCollector()
+    # isw_yesterday.add_url(isw_yesterday.generate_url_yesterday())
+    # a = isw_yesterday.urls[0]
+    # print(a)
+    # isw_req = ISWRequester(a[0])
 
-    isw_req.beautify()
-    b = isw_req.to_dict()
-    data_dict = isw_req.to_dict()
-    df_yesterday = pd.DataFrame(columns=data_dict.keys())
-    df_yesterday = pd.concat([df, pd.DataFrame([b])], ignore_index=True)
-    print(df_yesterday)
+    # isw_req.beautify()
+    # b = isw_req.to_dict()
+    # data_dict = isw_req.to_dict()
+    # df_yesterday = pd.DataFrame(columns=data_dict.keys())
+    # df_yesterday = pd.concat([df, pd.DataFrame([b])], ignore_index=True)
+    # print(df_yesterday)
 
 
-    df.dropna(subset=["date"], inplace=True)
-
-    print(df)
-
-    df.to_csv("ISW.csv", index=False)
-
+    # df.dropna(subset=["date"], inplace=True)
+    #
+    # print(df)
+    #
+    # df.to_csv("ISW.csv", index=False)
