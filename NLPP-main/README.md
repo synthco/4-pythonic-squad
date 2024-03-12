7a480698687dea06f729bddc8827a11921cc4c3c
synthco <94200878+synthco@users.noreply.github.com>
2024-03-12 19:32:14 +0200

    Updated with NLP starter pack and isw as package

---
+++ NLPP-main/README.md
@@ -0,0 +1,25 @@
+# NLP
+Projects and Content Related To NLP
+
+## Projects
+* Hotel Review Sentiment Analysis
+* Email Spam Detection
+* Twitter Twittes Sentiment Analysis
+* Stock Sentiment Analysis
+* Text Summrization (Extractive Approach,Web App) 
+* IMDB Movie Review Analysis
+* Fake News Detection
+
+
+## Basics Of NLP
+* Tokenizing
+* Stopwords
+* Stemming
+* Lemmatization
+* Word Net
+* CountVectorizer
+* TFIDF Vectorizer
+* Word Embedding (word2vec)
+
+A+rticle Link : https://medium.com/swlh/basics-of-natural-language-processing-in-10-minutes-2ed51e6d5d32
+
--- source/isw/isw_collector.py
+++ source/isw/isw_collector.py
@@ -1,6 +1,4 @@
 from isw_requester import ISWRequester
-import csv
-import pandas as pd
 import datetime as dt
 
 
@@ -72,57 +70,5 @@ class ISWCollector:
             yield dt.date.fromordinal(ordinal)
 
 
-if __name__ == "__main__":
-    isw_collector = ISWCollector()
-    isw_collector.add_url(isw_collector.generate_url_roca())
 
-    instances = []
-
-    for i in range(len(isw_collector.urls[0])):
-        url = isw_collector.urls[0][i]
-        r = ISWRequester(url)
-        instances.append(r)
-        print(f"url - {url}, status code - {r.title}")
-
-    with open("ISW01.csv", "w", newline="", encoding='utf-8') as csvfile:
-        a = instances[0]
-        data_dict = a.to_dict()
-        fieldnames = list(data_dict.keys())
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-
-        for instance in instances:
-            instance.beautify()
-            data_dict = instance.to_dict()
-            writer.writerow(data_dict)
-
-    df = pd.read_csv("ISW01.csv")
-
-    # print(df)
-
-    df.dropna(subset=["date"], inplace=True)
-
-    # print(df)
-
-    df.to_csv("ISW01.csv", index=False)
-
-    isw_yesterday = ISWCollector()
-    isw_yesterday.add_url(isw_yesterday.generate_url_yesterday())
-    a = isw_yesterday.urls[0]
-    # print(a)
-    isw_req = ISWRequester(a[0])
-
-    isw_req.beautify()
-    b = isw_req.to_dict()
-    data_dict = isw_req.to_dict()
-    df_yesterday = pd.DataFrame(columns=data_dict.keys())
-    df_yesterday = pd.concat([df, pd.DataFrame([b])], ignore_index=True)
-    # print(df_yesterday)
-
-    df.dropna(subset=["date"], inplace=True)
-
-    # print(df)
-
-    df.to_csv("ISW.csv", index=False)
 
