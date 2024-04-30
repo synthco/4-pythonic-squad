d365d74e20c35a3fd08284a7f62fd4dd3dc0beff
tsaebst <125909246+tsaebst@users.noreply.github.com>
2024-04-30 18:27:34 +0300

    Update dfender_test.py

--- Backend/API/dfender_test.py
+++ Backend/API/dfender_test.py
@@ -1,27 +1,3 @@
-# from dfender import Dfender
-# import subprocess
-#
-#
-# def test_dfender():
-#     dfender = Dfender()
-#     # df1 = dfender.isw_vector
-#     df2 = dfender.weather_vector
-#     #
-#     # df1.to_csv("ISW.csv", index=False)
-#     # df2.to_csv("Weather.csv", index=False)
-#     df = dfender.vector
-#     # df.to_csv('predict_vector.csv')
-#     print(dfender.result)
-#
-#     # print(dfender)
-#
-#
-# if __name__ == '__main__':
-#     # Виконати команду pip для встановлення пакету xgboost
-#     subprocess.run(["pip", "install", "xgboost"])
-#
-#     print("XGBoost successfully installed!")
-#     test_dfender()
 
 import os
 os.system('pip install flask')
