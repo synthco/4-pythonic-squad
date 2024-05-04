65dcb647866328c0bdf336e58440bfc31e0a03c6
Militokrat <107516785+Militokrat@users.noreply.github.com>
2024-05-04 19:36:43 +0300

    isw_requester.py

--- source/isw/isw_requester.py
+++ source/isw/isw_requester.py
@@ -41,8 +41,6 @@ class ISWRequester:
     def data_vect(self):
         return self._vector
 
-
-
     @staticmethod
     def gen_url_yesterday() -> str:
         base_url = "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment"
@@ -146,14 +144,11 @@ class ISWRequester:
 
         nlp = spacy.load("en_core_web_sm")
 
-
         # lemmatizing
         def lemmatize_text(text):
             doc = nlp(text)
             lemmatized_text = ' '.join([token.lemma_ for token in doc])
             return lemmatized_text
-
-
 
         # lowercasing
         def lowercase_text(text):
@@ -187,10 +182,8 @@ class ISWRequester:
 
         # stopword removal
         def remove_stopwords(text):
-            stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
-
-            ", ".join(stopwords)
-            STOPWORDS = set(stopwords)
+            ", ".join(stopwords.words('english'))
+            STOPWORDS = set(stopwords.words('english'))
             return " ".join([word for word in str(text).split() if word not in STOPWORDS])
 
         pure = self.df
@@ -215,7 +208,7 @@ class ISWRequester:
         # vectorizing
         #ENTER THE RIGHT PATH
 
-        with open("/Users/ivantyshchenko/Documents/GitHub/4-pythonic-squad/tfidf_vect.pkl", "rb") as f:
+        with open("tfidf_vect.pkl", "rb") as f:
             tfidf_vect = pickle.load(f)
 
         tfidf_matrix = tfidf_vect.transform(pure['text_for_vect2'])
